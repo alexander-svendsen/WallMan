857aadcf60c4f30cbd2e6d130546a5766752445a
asv009@post.uit.no <asv009@post.uit.no>
2013-08-22 15:20:40 +0200

    working game up with 2 players

--- graphics.py
+++ graphics.py
@@ -6,14 +6,14 @@ from pygame.locals import *
 import gamelayout
 import maps.config as gameLayoutConfig
 from graphics.wall import Wall
-from graphics.Player import Player as playerGraphics
+from graphics.player import Player as playerGraphics
 import settings
 from player import Player
 from graphics.floor import Floor
-
+import time
 
 BLOCK_SIZE = 24  # TODO MORE DYNAMIC
-
+playnameList = ["player1", "player2"]
 
 class WallManMain:
     """The Main WallMan Class"""
@@ -31,6 +31,7 @@ class WallManMain:
 
         self.width, self.height = res
         self.screen = pygame.display.set_mode(res, fullScreen)
+        self.players = list()
         pygame.display.set_caption("WallMan - Alexander Svendsen")
 
     def drawGameLayout(self):
@@ -48,14 +49,15 @@ class WallManMain:
                 centerPoint = [(x * BLOCK_SIZE) + x_offset, (y * BLOCK_SIZE + y_offset)]
                 blockData = layout[y][x]
                 if blockData == gameLayoutConfig.FLOOR:
-                    self.floorSprites.add(Floor(centerPoint, BLOCK_SIZE, BLOCK_SIZE, settings.FLOORCOLORS))
+                    self.floorSprites.add(Floor(centerPoint, BLOCK_SIZE, BLOCK_SIZE))
                 if blockData == gameLayoutConfig.BLOCK:
                     self.blockSprites.add(
                         Wall(centerPoint, settings.BLOCKCOLORS, BLOCK_SIZE, BLOCK_SIZE, settings.BLOCKWITH))
                 if blockData == gameLayoutConfig.PLAYER:
-                    self.player = Player(playerGraphics(centerPoint, BLOCK_SIZE, BLOCK_SIZE))
-                    self.playerSprites.add(self.player.getSprite())
-                    self.floorSprites.add(Floor(centerPoint, BLOCK_SIZE, BLOCK_SIZE, settings.FLOORCOLORS))
+                    player = Player(playerGraphics(centerPoint, BLOCK_SIZE, BLOCK_SIZE), playnameList.pop())
+                    self.players.append(player)
+                    self.playerSprites.add(player.getSprite())
+                    self.floorSprites.add(Floor(centerPoint, BLOCK_SIZE, BLOCK_SIZE))
         self.blockSprites.draw(self.screen)
 
     def main(self):
@@ -66,6 +68,7 @@ class WallManMain:
         self.drawGameLayout()
 
         running = True
+        time.clock()
         while running:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -74,13 +77,30 @@ class WallManMain:
                     if event.key == K_ESCAPE:
                         running = False
                     if event.key == K_RIGHT or event.key == K_LEFT or event.key == K_UP or event.key == K_DOWN:
-                        self.player.updateMovement(event.key)
-            self.player.update(self.blockSprites, self.floorSprites)
+                        self.players[0].updateMovement(event.key)
+                    if event.key == K_a or event.key == K_s or event.key == K_d or event.key == K_w:
+                        self.players[1].updateMovement(event.key)
             clock.tick(30)
+
+            for player in self.players:
+                player.update(self.blockSprites, self.floorSprites)
             self.floorSprites.draw(self.screen)
 
             self.playerSprites.draw(self.screen)
             pygame.display.flip()
+            timer = time.clock()
+            if timer > 30:
+                print "Game Over ..."
+                score = dict()
+                for floor in self.floorSprites:
+                    try:
+                        score[floor.getMarker()] += 1
+                    except KeyError:
+                        score[floor.getMarker()] = 1
+                print score
+                running = False
+            else:
+                print timer
 
     pygame.quit()
 
--- graphics/floor.py
+++ graphics/floor.py
@@ -2,18 +2,23 @@ import pygame
 
 
 class Floor(pygame.sprite.Sprite):
-    def __init__(self, centerPoint, width, height, color):
+    def __init__(self, centerPoint, width, height):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
         self.image.set_alpha(128)
         self.rect = self.image.get_rect()
-        self.color = color
 
-        """Move the rect into the correct position"""
+        # Move the rect into the correct position
         self.rect.center = centerPoint
+        self.drawingRect = pygame.Rect(width / 2 - width / 4, height / 2 - height / 4, width / 2, height / 2)
+        self.playerName = "None"
 
-    def mark(self):  # TODO , better update of the Floor
-        self.image.fill((255,255,255))
+    def mark(self, color, playerName):
+        self.image.fill(color, self.drawingRect)
+        self.playerName = playerName
+
+    def getMarker(self):
+        return self.playerName
 
     def unmark(self):
         pass
