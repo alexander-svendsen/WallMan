0c1aba258e1bef6149380535cb0eac3ce3fadd15
asv009@post.uit.no <asv009@post.uit.no>
2013-08-25 17:41:29 +0200

    added a simple example server, just to test things out

--- communication/server.py
+++ communication/server.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 import web
+import json
 
 urls = (
-    '/', 'Server'
+    '/(.*)', 'Server'
 )
 
 
-class Server:
+class Server:  #Single server controlling all states and players
     def GET(self):
         return "Hello, world!"
+
+    def POST(self, path=""):
+        if path == "join":
+            data = json.loads(web.data())
+            print data["name"]
+
 
 if __name__ == "__main__":
     app = web.application(urls, globals())
