26863a223ddf459ba7c1842cb12f62cf1c83452e
asv009@post.uit.no <asv009@post.uit.no>
2013-09-25 17:14:07 +0200

    refactored the player sprite

--- graphics/Player.py
+++ graphics/Player.py
@@ -4,24 +4,36 @@ import string
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, centerPoint, width, height):
+    def __init__(self, centerPoint, width, height, color=None, id=None, inversedColor=None):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
         self.rect = self.image.get_rect()
-        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        pygame.draw.rect(self.image, color, self.rect)
+
+        if color is None:
+            self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        else:
+            self.color = color
+        pygame.draw.rect(self.image, self.color, self.rect)
 
         myfont = pygame.font.SysFont("monospace", (width + height)/2 - 4)
         myfont.set_bold(1)
 
         #Inverse the colors
-        inverseColor = (255 - color[0], 255 - color[1], 255 - color[2])
-        label = myfont.render(self.idGenerator(2), 1, inverseColor)
+        if inversedColor is None:
+            self.inverseColor = (255 - self.color[0], 255 - self.color[1], 255 - self.color[2])
+        else:
+            self.inverseColor = inversedColor
+
+        if id is None:
+            self.id = self.idGenerator(2)
+        else:
+            self.id = id
+
+        label = myfont.render(self.id, 1, self.inverseColor)
         self.image.blit(label, (0, 0))
 
         """Move the rect into the correct position"""
         self.rect.center = centerPoint
-        self.color = color
 
     def idGenerator(self, size=6, chars=string.ascii_uppercase + string.digits):
         return ''.join(random.choice(chars) for x in range(size))
