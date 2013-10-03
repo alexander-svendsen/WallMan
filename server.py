334a6ca58af67edebd0129bdfe24c010ff60c783
asv009@post.uit.no <asv009@post.uit.no>
2013-10-03 18:22:44 +0200

    updatet the shell files

--- server.py
+++ server.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-import web
-import socketcommunication
 from server import server
-
 
 if __name__ == "__main__":
     server.main()
--- theGame.py
+++ theGame.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-import socketcommunication
 from gamelogic import main
-import maps
-import settings
-import graphics
 
 if __name__ == "__main__":
     main.main()
