32c0e25052f8626a2c3ae21bee5496f142f29911
asv009@post.uit.no <asv009@post.uit.no>
2013-10-06 14:49:46 +0200

    Updatet the .sh game file

--- game.sh
+++ game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Shellscript for runnning the game for linux users
 
-BASEDIR=$(dirname $BASH_SOURCE )
+BASEDIR=..
 export PYTHONPATH=$PYTHONPATH:$BASEDIR 	
 python gamelogic/main.py -m DisplayWallTest.json
