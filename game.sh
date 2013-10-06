67847719bafd8dbf4c6257a4c6f4c43e0d2ed92e
asv009@post.uit.no <asv009@post.uit.no>
2013-10-06 14:48:15 +0200

    Made a couplt of .bat files for running the code for windows users

--- game.sh
+++ game.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# My first script
-BASEDIR=$(dirname $BASH_SOURCE )
+# Shellscript for runnning the game for linux users
 
+BASEDIR=$(dirname $BASH_SOURCE )
 export PYTHONPATH=$PYTHONPATH:$BASEDIR 	
 python gamelogic/main.py -m DisplayWallTest.json
