c5c6782dc4fb3fd16882717b89873c8f33c2d5eb
Alexander Svendsen <alexander@svenhub.com>
2013-10-08 15:53:30 +0200

    The game.sh script now work dynamicly on the CF command

--- game.sh
+++ game.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# My first script
-BASEDIR $(cd $(dirname $0); pwd)
 # Shellscript for runnning the game for linux users
-export PYTHONPATH=$PYTHONPATH:$BASEDIR 	
+
+BASEDIR=$(cd $(dirname $0); pwd)
+export PYTHONPATH=$PYTHONPATH:$BASEDIR
+cd $BASEDIR
 python gamelogic/main.py -m DisplayWallTest.json
