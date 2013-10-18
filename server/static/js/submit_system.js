aeff21476599c1ff459b9571b2d8afd4a1f15539
asv009@post.uit.no <asv009@post.uit.no>
2013-10-18 20:01:02 +0200

    Made the start look as a terminal instead of the previous view. Mostly for fun. May add more functionality and a button for mobile users at another time

--- server/static/js/submit_system.js
+++ server/static/js/submit_system.js
@@ -1,27 +1,33 @@
 $("#playerProp").on("submit", function(e) {
     e.preventDefault();
-    var button = $('#joinButton');
-
+//    var button = $('#joinButton');
+//
     var inputField = $('#appendedInputButton');
-    var username = inputField.val();
+    var input = inputField.val();
 
-    if (username.length == 0){
-        button.toggleClass('btn-primary', false);
-        button.toggleClass('btn-danger', true);
-        $('#errorContainer').toggleClass('error', true);
-        var input = $('#appendedInputButton');
-        input.attr("placeholder", "Must have one");
-        input.css('border-color', '#b94a48');
+    var terminal = $('#console');
+    terminal.first().contents().first().remove();
+    while(terminal.first().contents().first()[0].nodeName == "#text"){
+        terminal.first().contents().first().remove();
+    }
+    if (input.length == 0){
+//        button.toggleClass('btn-primary', false);
+//        button.toggleClass('btn-danger', true);
+//        $('#errorContainer').toggleClass('error', true);
+//        var input = $('#appendedInputButton');
+//        input.attr("placeholder", "Must have one");
+//        input.css('border-color', '#b94a48');
+        terminal.append('<p class="error">Error: Must have a username</p>');
         return;
     }
-
-    button.prop('disabled', true);
+    terminal.append('<p>'+ input + '</p>');
+//    button.prop('disabled', true);
     inputField.prop('disabled', true);
 
-    button.toggleClass('btn-primary', true);
-    button.toggleClass('btn-danger', false);
-    $('#errorContainer').toggleClass('error', false);
-    ajax(tasksURI + '/join', 'POST', {"name" : username}).success(redirect(username));
+//    button.toggleClass('btn-primary', true);
+//    button.toggleClass('btn-danger', false);
+//    $('#errorContainer').toggleClass('error', false);
+    ajax(tasksURI + '/join', 'POST', {"name" : input}).success(redirect(input));
 });
 
 var redirect = function(username){
