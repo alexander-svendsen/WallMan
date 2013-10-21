0e69297ea68d16dc0b3188c3e979df49c82cd8af
asv009@post.uit.no <asv009@post.uit.no>
2013-10-21 12:43:13 +0200

    Added a enter button to the start page and cleaned up the code a little to be more mobile friendly

--- server/static/js/submit_system.js
+++ server/static/js/submit_system.js
@@ -1,7 +1,5 @@
 $("#playerProp").on("submit", function(e) {
     e.preventDefault();
-//    var button = $('#joinButton');
-//
     var inputField = $('#appendedInputButton');
     var input = inputField.val();
 
@@ -11,27 +9,17 @@ $("#playerProp").on("submit", function(e) {
         terminal.first().contents().first().remove();
     }
     if (input.length == 0){
-//        button.toggleClass('btn-primary', false);
-//        button.toggleClass('btn-danger', true);
-//        $('#errorContainer').toggleClass('error', true);
-//        var input = $('#appendedInputButton');
-//        input.attr("placeholder", "Must have one");
-//        input.css('border-color', '#b94a48');
         terminal.append('<p class="error">Error: Must have a username</p>');
         return;
     }
     terminal.append('<p>'+ input + '</p>');
-//    button.prop('disabled', true);
     inputField.prop('disabled', true);
 
-//    button.toggleClass('btn-primary', true);
-//    button.toggleClass('btn-danger', false);
-//    $('#errorContainer').toggleClass('error', false);
     ajax(tasksURI + '/join', 'POST', {"name" : input}).success(redirect(input));
 });
 
 var redirect = function(username){
     $.cookie("username", username);
     $.cookie("refresh", true);
-    window.location.replace("/static/control.html");
+    window.location.href = "/static/control.html";
 };
